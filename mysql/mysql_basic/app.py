0dbe97375dd567e7d0bf6192c3cfe49ad8c8788e
Eirik Tennøfjord <eirik@tenno.no>
2020-11-27 11:48:29 +0100

    python-mysql fiddle with mysql workbench

--- mysql/mysql_basic/app.py
+++ mysql/mysql_basic/app.py
@@ -1,31 +1,44 @@
 import mysql.connector
-
+import datetime
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="1234",
-    database="test"
+    password="!1234Asdf",
+    database="3elda"
 )
 
-print(mydb)
-
-
 mycursor = mydb.cursor()
+
+print("Connected..")
+
+sql = "INSERT INTO sensor(verdi,tid) VALUES (%s,%s)"
+
+verdi = 5.0
+tid = datetime.datetime.now()
+
+val = (verdi, tid)
+
+print("Executing...")
+
+mycursor.execute(sql, val)
+mydb.commit()
+
+print("Done")
 
 '''
     Inserting to database:
     
     
 '''
-
-sql = "INSERT INTO sensors (name) VALUES ute_temp"
-val = ("ute_temp")
-
-mycursor.execute(sql)
-mydb.commit()
-
-print(mycursor.rowcount, "record inserted")
+#
+# sql = "INSERT INTO sensors (name) VALUES ute_temp"
+# val = ("ute_temp")
+#
+# mycursor.execute(sql)
+# mydb.commit()
+#
+# print(mycursor.rowcount, "record inserted")
 
 '''
     Show tables:------------------
