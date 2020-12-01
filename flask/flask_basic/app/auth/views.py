830e0a2b472b5dad6387f0d08768295bce601d62
Eirik Tennøfjord <eirik@tenno.no>
2020-12-01 20:57:03 +0100

    Updated register, login and logout functions.

--- flask/flask_basic/app/auth/views.py
+++ flask/flask_basic/app/auth/views.py
@@ -3,34 +3,43 @@ from .forms import LoginForm, RegisterForm
 from ..models import User
 from .. import db
 from flask import redirect, url_for, render_template
+from flask_login import login_user, logout_user, current_user
 
 
 @auth.route('/')
-def auth_index():
-    return 'Auth Index'
+def index():
+	return 'Auth Index'
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        print("123456")
-        user = User(
-            email=form.email.data,
-            username=form.username.data,
-            password=form.password.data
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('main.index'))
-    return render_template('auth/register.html', form=form)
+	print("test1")
+	form = RegisterForm()
+	if form.validate_on_submit():
+		print('test2')
+		user = User(
+			username=form.username.data,
+			email=form.email.data,
+			password=form.password.data
+		)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('main.index'))
+	return render_template('auth/register.html', form=form)
 
 
-@auth.route('/login', methods=['GET'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'LOGIN'
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user, form.remember_me)
+			return redirect(url_for('main.index'))
+	return render_template('auth/login.html', form=form)
 
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+	logout_user()
+	return redirect(url_for('main.index'))
