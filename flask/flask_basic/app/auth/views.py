97066eef56fbbe002d8dbf832e5fc3f65ebd2663
Eirik Tennøfjord <eirik@tenno.no>
2020-12-01 15:18:52 +0100

    Added forms, views, and templates for auth. Register not yet working.

--- flask/flask_basic/app/auth/views.py
+++ flask/flask_basic/app/auth/views.py
@@ -1,6 +1,36 @@
 from . import auth
+from .forms import LoginForm, RegisterForm
+from ..models import User
+from .. import db
+from flask import redirect, url_for, render_template
 
 
 @auth.route('/')
 def auth_index():
     return 'Auth Index'
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print("123456")
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form=form)
+
+
+@auth.route('/login', methods=['GET'])
+def login():
+    return 'LOGIN'
+
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
--- flask/flask_basic/app/models.py
+++ flask/flask_basic/app/models.py
@@ -1,11 +1,26 @@
-from . import db
+from . import db, login_manager
 from app.exceptions import ValidationError
+from flask_login import UserMixin, AnonymousUserMixin, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, index=True, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     @staticmethod
     def to_json(self):
@@ -20,3 +35,15 @@ class User(db.Model):
         if username is None:
             raise ValidationError('Empty username')
         return User(username=username)
+
+
+class AnonymousUser(AnonymousUserMixin):
+    pass
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+login_manager.anonymous_user = AnonymousUser
