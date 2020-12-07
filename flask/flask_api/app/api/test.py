dfe85435a5ac19968d1f7180cb243665af45b9c2
Eirik Tennøfjord <eirik@tenno.no>
2020-12-07 13:35:12 +0100

    Flask-API: Small update

--- flask/flask_api/app/api/test.py
+++ flask/flask_api/app/api/test.py
@@ -4,9 +4,17 @@ from ..models import User
 from .. import db
 
 
+# HTTPIE: http --json localhost:5000/api/ "username=<input>"
 @api.route('/', methods=['POST'])
 def new_user():
     user = User.from_json(request.json)
     db.session.add(user)
     db.session.commit()
+    return jsonify(user.to_json(user))
+
+
+@api.route('/load_user/', methods=['POST'])
+def load_user():
+    username = request.json.get('username')
+    user = User.query.filter_by(username=username).first()
     return jsonify(user.to_json(user))
